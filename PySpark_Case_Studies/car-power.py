99b90f98417826353f10295c641a9c8b7cd04cfa
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-12 23:24:08 -0500

    Add column headers

--- PySpark_Case_Studies/car-power.py
+++ PySpark_Case_Studies/car-power.py
@@ -7,3 +7,5 @@ if __name__ == "__main__":
         ("Chevrolet Monte Carlo", 150, 3761, "US"),
         ("BMW 2002", 113, 2234, "Europe")
     ]
+
+    columns = ["carr", "horsepower", "weight", "origin"]
