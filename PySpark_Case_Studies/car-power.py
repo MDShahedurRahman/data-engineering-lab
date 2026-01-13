63ef7069c978705d3e9cba12decc78294f275916
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-12 23:23:41 -0500

    Add data

--- PySpark_Case_Studies/car-power.py
+++ PySpark_Case_Studies/car-power.py
@@ -2,3 +2,8 @@ from pyspark.sql import SparkSession
 
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("Car Power").master("local[*]").getOrCreate()
+    data = [
+        ("Ford Torino", 140, 3449, "US"),
+        ("Chevrolet Monte Carlo", 150, 3761, "US"),
+        ("BMW 2002", 113, 2234, "Europe")
+    ]
