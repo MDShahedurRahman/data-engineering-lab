d2eac46d685abdbab2f7586a55b382ed763fbc76
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-12 23:14:50 -0500

    Implement Read from Hadoop

--- PySpark_Case_Studies/covid19.py
+++ PySpark_Case_Studies/covid19.py
@@ -3,3 +3,7 @@ from pyspark.sql import functions as Func
 
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("covid19").master("local[*]").enableHiveSupport().getOrCreate()
+
+    # Read from Hadoop
+    df = spark.read.options(header='True', inferSchema='True', delimiter=',') \
+        .csv("/data/spark/covid19/CovidCases.csv")
