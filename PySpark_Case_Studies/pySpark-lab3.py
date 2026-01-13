628cebb4cbd33ab8aded4a494a6eb79f9b8e9a92
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-13 14:11:51 -0500

    Read the JSON file into a DataFrame

--- PySpark_Case_Studies/pySpark-lab3.py
+++ PySpark_Case_Studies/pySpark-lab3.py
@@ -2,3 +2,6 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, avg
 
 spark = SparkSession.builder.appName("Employee_Q3").master("local[*]").getOrCreate()
+
+# Read the JSON file into a DataFrame
+df = spark.read.json("/user/test/data/employee.json")
