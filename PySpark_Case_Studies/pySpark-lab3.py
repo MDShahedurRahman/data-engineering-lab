03cee8e1669d31abb3ec291291a1ba7deb488dfe
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-13 14:12:39 -0500

    Write ORC partitioned by department

--- PySpark_Case_Studies/pySpark-lab3.py
+++ PySpark_Case_Studies/pySpark-lab3.py
@@ -8,3 +8,6 @@ df = spark.read.json("/user/test/data/employee.json")
 
 # Distinct employees
 dist_df = df.dropDuplicates()
+
+# Write ORC partitioned by department
+dist_df.write.mode("overwrite").format("orc").partitionBy("department").save("/user/test/output/employee_orc_partByDept")
