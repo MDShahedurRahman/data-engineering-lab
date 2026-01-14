5981364a2de1a5caad049cdd841b5364c38d7d37
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-14 01:48:27 -0500

    Read the JSON file into a DataFrame

--- PySpark_Case_Studies/pySpark-lab4.py
+++ PySpark_Case_Studies/pySpark-lab4.py
@@ -2,3 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, max as spark_max, count
 
 spark = SparkSession.builder.appName("Employee-Department_Q4").master("local[*]").enableHiveSupport().getOrCreate()
+
+# Read the JSON file into a DataFrame
+employee_df = spark.read.json("/user/data/employee.json")
+department_df = spark.read.json("/user/data/department.json")
