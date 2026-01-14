f13116f81a93db1172f661775849862e200402ec
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-14 01:54:31 -0500

    Read the CSV file into a DataFrame

--- PySpark_Case_Studies/pySpark-lab5.py
+++ PySpark_Case_Studies/pySpark-lab5.py
@@ -2,3 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
 spark = SparkSession.builder.appName("Zipcodes_Q5").master("local[*]").enableHiveSupport().getOrCreate()
+
+# Read the CSV file into a DataFrame
+df = (spark.read.option("header", "true").option("inferSchema", "true")
+      .csv("/user/test/data/simple-zipcodes.csv"))
