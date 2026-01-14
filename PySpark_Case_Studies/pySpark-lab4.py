53240421450a1c5e1bb883c3faa5638829b031e2
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-14 01:48:49 -0500

    join employee and department dataframes

--- PySpark_Case_Studies/pySpark-lab4.py
+++ PySpark_Case_Studies/pySpark-lab4.py
@@ -6,3 +6,7 @@ spark = SparkSession.builder.appName("Employee-Department_Q4").master("local[*]"
 # Read the JSON file into a DataFrame
 employee_df = spark.read.json("/user/data/employee.json")
 department_df = spark.read.json("/user/data/department.json")
+
+# join employee and department dataframes
+joined_df = (employee_df.join(department_df, employee_df["emp_dept_id"].cast("int") == department_df["dept_id"], "inner")
+            .select(department_df["dept_name"], employee_df["salary"].cast("long").alias("salary")))
