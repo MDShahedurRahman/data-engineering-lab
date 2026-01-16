9707a6c2bae2a884d7ce9fe7e034e96120a41d55
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-15 23:09:38 -0500

    Define the Hive database name

--- lab-02/silver_transforms_lab.py
+++ lab-02/silver_transforms_lab.py
@@ -1,7 +1,9 @@
 from pyspark.sql import SparkSession  # Import SparkSession to create and manage Spark applications
 
-
 def main():
+    # Define the Hive database name
+    DB_NAME = "iphone_analytics"
+
 
 # Entry point of the Python script
 if __name__ == "__main__":
