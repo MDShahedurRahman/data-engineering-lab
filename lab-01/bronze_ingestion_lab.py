de4e086b083f7ccc91b523f70559219979bff17a
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-15 22:24:55 -0500

    Add Spark Session

--- lab-01/bronze_ingestion_lab.py
+++ lab-01/bronze_ingestion_lab.py
@@ -1,12 +1,16 @@
 from pyspark.sql import SparkSession
 
 def main():
-    # Name of the Hive database to store tables
     DB_NAME = "iphone_analytics"
-
-    # Base directory where raw CSV files are stored
     BASE_PATH = "/user/md/iphone/raw_data"
 
-# Entry point of the script
+    spark = (
+        SparkSession.builder
+        .master("local[*]")
+        .appName("iphone-bronze-ingestion")
+        .enableHiveSupport()
+        .getOrCreate()
+    )
+
 if __name__ == "__main__":
     main()
