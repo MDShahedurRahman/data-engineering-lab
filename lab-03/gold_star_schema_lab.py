9b8a69d0500890650c5eed4342636ddc54937f93
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-16 21:32:40 -0500

    create a data frame to join sales with products and calculate total_amount

--- lab-03/gold_star_schema_lab.py
+++ lab-03/gold_star_schema_lab.py
@@ -9,6 +9,22 @@ def main():
     # Read product data to calculate total sales amount
     products = spark.table("silver_products")
 
+    # Join sales with products and calculate total_amount
+    fact_df = (
+        sales.join(products, "product_id")  # Join on product_id
+        .withColumn("total_amount",  # Calculate revenue per sale
+                    col("quantity") * col("unit_price"))
+        .select(
+            "sale_id",
+            "customer_id",
+            "product_id",
+            "store_id",
+            col("sale_date").alias("date_key"),  # Rename sale_date for partitioning
+            "quantity",
+            "total_amount"
+        )
+    )
+
 # Script entry point
 if __name__ == "__main__":
     main()
